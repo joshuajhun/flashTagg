0771be1e0d01ea8abd678e6ba814a2f98a68b5b0
joshuajhun <joshuajhun@gmail.com>
2016-03-03 11:48:08 -0700

    server is now up and running. installed dependences such as sockets.io and express

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const http = require('http');
-const express = require('express')
+const http     = require('http');
+const express  = require('express');
+const app      = express()
+var server     = http.createServer(app)
+const socketIo = require('socket.io');
+const io       = socketIo(server);
+var port       = process.env.PORT || 3000;
 
-const app = express();
 
-app.user(express.static('public'))
+server.listen(port, function(){
+  console.log('Listening on port' + port + '.');
+});
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFil(__dirname + '/public/index.html');
+})
+
+
+module.exports = server;
